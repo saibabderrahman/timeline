50a908c822a33b329bfd452702753bd900d87217
abderrahman <saibabderrahman@gmail.com>
2023-03-02 00:24:39 -0800

    first one

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -11,60 +11,112 @@ import Timeline, {
 
 } from 'react-calendar-timeline'
 import moment from 'moment'
+import Svg from './Svg'
+import img from './antonio-perdichizzi.webp'
 
 
 const DTimeline = () => {
-    const groups = [{ id: 1, title: 'raid dj' ,    tip: 'additional information'
-}, { id: 2, title: 'walide ' ,    tip: 'additional information'
-},{ id: 3, title: 'raid dj' ,    tip: 'additional information'
-}, { id: 4, title: 'walide ' ,    tip: 'additional information'
-},{ id: 5, title: 'raid dj' ,    tip: 'additional information'
-}, { id: 6, title: 'walide ' ,    tip: 'additional information'
-}]
+    const groups = [{ id: 1, title: 'raid dj' ,    rightTitle: false,  height: 60},
+       { id: 2, title: `walide` ,    rightTitle: true, img:'' },
+      { id: 3, title: 'raid dj' ,    rightTitle: true,  },
+       { id: 4, title: 'walide ' ,    rightTitle: true, },
+       { id: 5, title: 'raid dj' ,    rightTitle: false, },
+        { id: 6, title: 'walide ' ,    rightTitle: true},
+       { id: 7, title: 'walide ' ,    rightTitle: true}, 
+        { id: 8, title: 'walide ' ,    rightTitle: false},
+        { id: 9, title: 'walide ' ,    rightTitle: true},
+        { id: 10, title: 'walide ' ,    rightTitle: true},
+        { id: 11, title: 'walide ' ,    rightTitle: false},
+        { id: 12, title: 'walide ' ,    rightTitle: false},
+        { id: 13, title: 'walide ' ,    rightTitle: false},
+        { id: 13, title: 'walide ' ,    rightTitle: true},
+        { id: 14, title: 'walide ' ,    rightTitle: true},
+        { id: 15, title: 'walide ' ,    rightTitle: true},
+        { id: 16, title: 'walide ' ,    rightTitle: true},
+        { id: 17, title: 'walide ' ,    rightTitle: true},
+        { id: 18, title: 'walide ' ,    rightTitle: true},
+        { id: 19, title: 'walide ' ,    rightTitle: true},
+        { id: 20, title: 'walide ' ,    rightTitle: true},
+        { id: 21, title: 'walide ' ,    rightTitle: true},
+        { id: 22, title: 'walide ' ,    rightTitle: true},
+]
 
   const items = [
     {
       id: 1,
       group: 1,
-      title: 'item 1',
-      descption:"thisbis jsut a test",
+      title: 'additional information',
       start_time: moment(),
-      end_time: moment().add(1, 'hour')
+      end_time: moment().add(1, 'hour'),
+      rightTitle: 'title in the right sidebar',
+
+      Color:"blue"
     },
     {
       id: 2,
       group: 2,
-      title: 'item 2',
+      title: 'additional information',
+      rightTitle: 'title in the right sidebar',
       start_time: moment().add(1, 'hour'),
       end_time: moment().add(6, 'hour')
     },
     {
       id: 3,
       group: 1,
-      title: 'item 3',
+      title: 'additional information',
       start_time: moment().add(2, 'hour'),
       end_time: moment().add(4, 'hour')
     },
     {
         id: 4,
         group: 4,
-        title: 'item 3',
+        title: 'onligne',
         start_time: moment().add(-2, 'hour'),
-        end_time: moment().add(2, 'hour')
+        end_time: moment().add(2, 'hour'),
+        itemProps: {
+          // these optional attributes are passed to the root <div /> of each item as <div {...itemProps} />
+          'data-custom-attribute': 'Random content',
+          'aria-hidden': true,
+          onDoubleClick: () => { console.log('You clicked double!') },
+          className: 'weekend',
+          style: {
+            background: 'green'
+          }
+        }
       },
       {
         id: 5,
         group: 5,
-        title: 'item 3',
+        title: 'additional information',
         start_time: moment().add(-5, 'hour'),
-        end_time: moment().add(2, 'hour')
+        end_time: moment().add(2, 'hour'),
+        itemProps: {
+          // these optional attributes are passed to the root <div /> of each item as <div {...itemProps} />
+          'data-custom-attribute': 'Random content',
+          'aria-hidden': true,
+          onDoubleClick: () => { console.log('You clicked double!') },
+          className: 'weekend',
+          style: {
+            background: 'blue'
+          }
+        }
       },
       {
         id: 6,
         group: 1,
-        title: 'item 3',
+        title: 'additional information',
         start_time: moment().add(-5, 'hour'),
-        end_time: moment().add(-2, 'hour')
+        end_time: moment().add(-2, 'hour'),
+        itemProps: {
+          // these optional attributes are passed to the root <div /> of each item as <div {...itemProps} />
+          'data-custom-attribute': 'Random content',
+          'aria-hidden': true,
+          onDoubleClick: () => { console.log('You clicked double!') },
+          className: 'weekend',
+          style: {
+            background: 'black'
+          }
+        }
       },
 
 
@@ -75,121 +127,98 @@ const DTimeline = () => {
     <div>
                 <Timeline
          traditionalZoom={false}
+         
+
          canMove={ true}
          canResize={ true}
          canChangeGroup={ true}
-         groups={(groups)}
+         groups={groups}
          items={items}
          defaultTimeStart={moment().add(-12, 'hour')}
          defaultTimeEnd={moment().add(12, 'hour')}
          onDoubleClick=  {() => { console.log('You clicked double!') }}
-         sidebarWidth={150}
+         sidebarWidth={200}
          lineHeight={60}
          Height={50}
          timelineWidth={30}
+         minZoom={60}
+         maxZoom={60}
+         scrollRef={()=>5}
+         groupRenderer = {({ group }) => {
+          return (
+            <>
+            <div className="custom-group">
+{group.rightTitle ? 
+ <div class="container">
+ <span></span>
+</div>:  <div class="con">
+     <span></span>
+ </div>}
+ <div className='userCart'>
+
+ <span className="title">{group.title}</span>
+ <img src={img} class="photo" />
+ </div>
+
+
+</div>
+                
+                
+                
+
+            
+            
+            
+            </>
+
+
+
+
+    
+          
+
+
+
+
+
+
+
+
+
+
+          )
+        }}
+        
+        
+
+
 
 
     >
         <TimelineHeaders>
-    <SidebarHeader>
-      {({ getRootProps }) => {
-        return <div >
-            		<svg class="pl" viewBox="0 0 160 160" width="160px" height="160px" xmlns="http://www.w3.org/2000/svg">
-			<defs>
-				<linearGradient id="grad" x1="0" y1="0" x2="0" y2="1">
-					<stop offset="0%" stop-color="#000"></stop>
-					<stop offset="100%" stop-color="#fff"></stop>
-				</linearGradient>
-				<mask id="mask1">
-					<rect x="0" y="0" width="160" height="160" fill="url(#grad)"></rect>
-				</mask>
-				<mask id="mask2">
-					<rect x="28" y="28" width="104" height="104" fill="url(#grad)"></rect>
-				</mask>
-			</defs>
-			
-			<g>
-				<g class="pl__ring-rotate">
-					<circle class="pl__ring-stroke" cx="80" cy="80" r="72" fill="none" stroke="hsl(223,90%,55%)" stroke-width="16" stroke-dasharray="452.39 452.39" stroke-dashoffset="452" stroke-linecap="round" transform="rotate(-45,80,80)"></circle>
-				</g>
-			</g>
-			<g mask="url(#mask1)">
-				<g class="pl__ring-rotate">
-					<circle class="pl__ring-stroke" cx="80" cy="80" r="72" fill="none" stroke="hsl(193,90%,55%)" stroke-width="16" stroke-dasharray="452.39 452.39" stroke-dashoffset="452" stroke-linecap="round" transform="rotate(-45,80,80)"></circle>
-				</g>
-			</g>
-			
-			<g>
-				<g stroke-width="4" stroke-dasharray="12 12" stroke-dashoffset="12" stroke-linecap="round" transform="translate(80,80)">
-					<polyline class="pl__tick" stroke="hsl(223,10%,90%)" points="0,2 0,14" transform="rotate(-135,0,0) translate(0,40)"></polyline>
-					<polyline class="pl__tick" stroke="hsl(223,10%,90%)" points="0,2 0,14" transform="rotate(-90,0,0) translate(0,40)"></polyline>
-					<polyline class="pl__tick" stroke="hsl(223,10%,90%)" points="0,2 0,14" transform="rotate(-45,0,0) translate(0,40)"></polyline>
-					<polyline class="pl__tick" stroke="hsl(223,10%,90%)" points="0,2 0,14" transform="rotate(0,0,0) translate(0,40)"></polyline>
-					<polyline class="pl__tick" stroke="hsl(223,10%,90%)" points="0,2 0,14" transform="rotate(45,0,0) translate(0,40)"></polyline>
-					<polyline class="pl__tick" stroke="hsl(223,10%,90%)" points="0,2 0,14" transform="rotate(90,0,0) translate(0,40)"></polyline>
-					<polyline class="pl__tick" stroke="hsl(223,10%,90%)" points="0,2 0,14" transform="rotate(135,0,0) translate(0,40)"></polyline>
-					<polyline class="pl__tick" stroke="hsl(223,10%,90%)" points="0,2 0,14" transform="rotate(180,0,0) translate(0,40)"></polyline>
-				</g>
-			</g>
-			<g mask="url(#mask1)">
-				<g stroke-width="4" stroke-dasharray="12 12" stroke-dashoffset="12" stroke-linecap="round" transform="translate(80,80)">
-					<polyline class="pl__tick" stroke="hsl(223,90%,80%)" points="0,2 0,14" transform="rotate(-135,0,0) translate(0,40)"></polyline>
-					<polyline class="pl__tick" stroke="hsl(223,90%,80%)" points="0,2 0,14" transform="rotate(-90,0,0) translate(0,40)"></polyline>
-					<polyline class="pl__tick" stroke="hsl(223,90%,80%)" points="0,2 0,14" transform="rotate(-45,0,0) translate(0,40)"></polyline>
-					<polyline class="pl__tick" stroke="hsl(223,90%,80%)" points="0,2 0,14" transform="rotate(0,0,0) translate(0,40)"></polyline>
-					<polyline class="pl__tick" stroke="hsl(223,90%,80%)" points="0,2 0,14" transform="rotate(45,0,0) translate(0,40)"></polyline>
-					<polyline class="pl__tick" stroke="hsl(223,90%,80%)" points="0,2 0,14" transform="rotate(90,0,0) translate(0,40)"></polyline>
-					<polyline class="pl__tick" stroke="hsl(223,90%,80%)" points="0,2 0,14" transform="rotate(135,0,0) translate(0,40)"></polyline>
-					<polyline class="pl__tick" stroke="hsl(223,90%,80%)" points="0,2 0,14" transform="rotate(180,0,0) translate(0,40)"></polyline>
-				</g>
-			</g>
-			
-			<g>
-				<g transform="translate(64,28)">
-					<g class="pl__arrows" transform="rotate(45,16,52)">
-						<path fill="hsl(3,90%,55%)" d="M17.998,1.506l13.892,43.594c.455,1.426-.56,2.899-1.998,2.899H2.108c-1.437,0-2.452-1.473-1.998-2.899L14.002,1.506c.64-2.008,3.356-2.008,3.996,0Z"></path>
-						<path fill="hsl(223,10%,90%)" d="M14.009,102.499L.109,58.889c-.453-1.421,.559-2.889,1.991-2.889H29.899c1.433,0,2.444,1.468,1.991,2.889l-13.899,43.61c-.638,2.001-3.345,2.001-3.983,0Z"></path>
-					</g>
-				</g>
-			</g>
-			<g mask="url(#mask2)">
-				<g transform="translate(64,28)">
-					<g class="pl__arrows" transform="rotate(45,16,52)">
-						<path fill="hsl(333,90%,55%)" d="M17.998,1.506l13.892,43.594c.455,1.426-.56,2.899-1.998,2.899H2.108c-1.437,0-2.452-1.473-1.998-2.899L14.002,1.506c.64-2.008,3.356-2.008,3.996,0Z"></path>
-						<path fill="hsl(223,90%,80%)" d="M14.009,102.499L.109,58.889c-.453-1.421,.559-2.889,1.991-2.889H29.899c1.433,0,2.444,1.468,1.991,2.889l-13.899,43.61c-.638,2.001-3.345,2.001-3.983,0Z"></path>
-					</g>
-				</g>
-			</g>
-		</svg>
-        </div>
-      }}
-    </SidebarHeader>
-    <DateHeader unit="year" width='100' />
-    <DateHeader unit="month" width='100' />
-    <DateHeader unit="day" width='100' />
-    <DateHeader unit="hour" width='100' />
- 
-  </TimelineHeaders>
+          
+              <DateHeader unit="hour" height={60} 
+                         // style={{ background: 'green' }}
+                 
+               
+               />
+     
+         </TimelineHeaders>
 
 
         <TimelineMarkers>
-    <TodayMarker />
-    <CustomMarker date={moment()} />
+                <TodayMarker >
+                                  {({ styles, date }) => {
+                         const customStyles = {
+                           ...styles,
+                           backgroundColor: 'red',
+                           width: '4px'
+                         }
+                         return <div style={customStyles}  />
+                  }}
 
-    <CustomMarker date={moment().add(24, 'hour')}>
-      {/* custom renderer for this marker */}
-      {({ styles, date }) => {
-        const customStyles = {
-          ...styles,
-          backgroundColor: 'red',
-          width: '4px'
-        }
-        return <div style={customStyles}  />
-      }}
-      
-    </CustomMarker>
-    <CursorMarker />
-  </TimelineMarkers>
+                </TodayMarker>
+       </TimelineMarkers>
 
       </Timeline>
 
@@ -200,3 +229,48 @@ const DTimeline = () => {
 }
 
 export default DTimeline
+/*
+
+<div class="card">
+  <div class="card-top-part">
+    <div class="left-part">
+      <div class="user-name">
+        <p class="name">Jane Doe</p>
+        <p class="role"> Admin </p>
+      </div>
+      <div class="user-position">
+        <p class="position">
+        Front-End Developer
+      </p>
+      </div>
+    </div>
+    <div class="right-part">
+      <div class="user-photo">
+        <img src="https://randomuser.me/api/portraits/women/79.jpg" class="photo">
+      </div>
+    </div>
+  </div>
+  <div class="card-bottom-part">
+    <div class="bottom-part">
+      <a href="mailto: example@example.com" class="link">
+        <span class="icon"> 
+          <svg viewBox="0 0 20 20" height="20" width="20" xmlns="http://www.w3.org/2000/svg" data-name="20" id="_20">
+            <path transform="translate(1.25 3.75)" d="M16.25,12.5h-15A1.252,1.252,0,0,1,0,11.25v-10A1.252,1.252,0,0,1,1.25,0h15A1.251,1.251,0,0,1,17.5,1.25v10A1.251,1.251,0,0,1,16.25,12.5ZM1.25,1.819V11.25h15V1.819L9.106,6.763a.626.626,0,0,1-.713,0ZM2.625,1.25,8.75,5.487,14.875,1.25Z" id="Fill"></path>
+          </svg>
+        </span>
+          Email
+      </a>
+    </div>
+    <div class="bottom-part">
+      <a href="tel: 0123456789" class="link">
+        <span class="icon"> 
+          <svg viewBox="0 0 20 20" height="20" width="20" xmlns="http://www.w3.org/2000/svg" data-name="20" id="_20">
+            <path transform="translate(1.869 1.875)" d="M14.381,16.25h-.106C2,15.544.249,5.179.006,2.019A1.874,1.874,0,0,1,1.731,0H5.175A1.243,1.243,0,0,1,6.337.787l.95,2.337a1.247,1.247,0,0,1-.275,1.35L5.681,5.818a5.875,5.875,0,0,0,4.738,4.75l1.356-1.344a1.25,1.25,0,0,1,1.356-.257l2.356.944a1.245,1.245,0,0,1,.769,1.163v3.3A1.877,1.877,0,0,1,14.381,16.25Zm-12.5-15a.625.625,0,0,0-.625.625v.05C1.545,5.648,3.4,14.375,14.343,15h.038a.625.625,0,0,0,.625-.589V11.075l-2.356-.944-1.794,1.781-.3-.038A6.733,6.733,0,0,1,5.429,8.553,8.171,8.171,0,0,1,4.381,5.7l-.038-.3L6.118,3.606,5.181,1.25Z" id="Fill"></path>
+          </svg>
+        </span>
+          Phone
+      </a>
+    </div>
+  </div>
+</div>
+*/
